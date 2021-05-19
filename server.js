a1600edaa17d7b9eb472c4eb03f7ac41efc195a6
Shashi Bhagat <skujur871@gmail.com>
2021-05-19 12:47:45 +0530

    Added db and schema and model

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const Event = require('./models/event');
 
 app.use(express.json());
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]
+            events: [Event!]! 
         }
 
         type RootMutation {
-            createEvents(name: String): String
+            createEvents(eventInput: EventInput): Event
         }
 
         schema {
@@ -22,7 +40,35 @@ app.use('/graphql', graphqlHTTP({
         }
 
     `),
-    rootValue: null,
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvents: (args) => {
+            // const event = {
+            //     _id: Math.random().toString,
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date
+            // }
+
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            })
+            return event.save().then(result => {
+                console.log(result);
+                return {...result._doc};
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            })
+            return event;
+        }
+    },
     graphiql: true
 }))
 
@@ -30,4 +76,13 @@ app.get('/', (req, res) => {
     res.send('<h4>Welcome this is your home page</h4>')
 })
 
-app.listen(4000, () => console.log('server started at 4000'));
+// Connection to Servers 
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql.rna1k.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        app.listen(4000, () => console.log('server started at 4000'));
+    })
+    .catch(err => {
+        console.log(err);
+    })
